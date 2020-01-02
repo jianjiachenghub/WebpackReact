5793fe95839710b8eccf22ce758c91b71f776400
jianjiachengg <1797731542@qq.com>
2020-01-02 13:43:19 +0800

    添加分割代码配置

--- config/webpack.prod.config.js
+++ config/webpack.prod.config.js
@@ -86,16 +86,29 @@ module.exports = merge(common, {
     ],
     splitChunks: {// 抽离公共代码 具体配置看官网
       chunks: 'all',// 效值是all、async和initial。提供all可能特别强大，因为这意味着即使在异步和非异步块之间也可以共享块
-      minSize: 30000,
+      minSize: 30000,// 提出chunk的最小大小 30KB
       maxSize: 0,
       minChunks: 1,
       cacheGroups: {// 定义了被抽离的模块如何分成组，不然公共代码全打包到一个JS文件里面
+        component: {  //拆分指定文件
+          test: /(src\/component)/,
+          name: 'component',
+          chunks: 'initial',
+          priority: 2
+        },
         vendors: {// 第三方库抽离
           priority: 1,// 权重 先进行第三方库抽离
           test:  /[\\/]node_modules[\\/]/,// 选从node_modules文件夹下引入的模块，所以所有第三方模块才会被拆分出来 递归的
           name: "vendor",
+          chunks: 'initial',
           enforce: true,
         },
+        default: {
+          name: 'common',
+          chunks: 'initial',
+          minChunks: 2,  //模块被引用2次以上的才抽离
+          priority: -1 
+      },
       }
     }
   }
